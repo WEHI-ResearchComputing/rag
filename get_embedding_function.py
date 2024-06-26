3207e71b94114cc483fdf297da2f740db4978868
Edward Yang <yang.e@wehi.edu.au>
2024-06-26 12:43:45 +1000

    use hf in embed func instead of ollama

--- get_embedding_function.py
+++ get_embedding_function.py
@@ -1,12 +1,23 @@
 from langchain_community.embeddings.ollama import OllamaEmbeddings
 from langchain_community.embeddings.bedrock import BedrockEmbeddings
+from langchain_community.embeddings.ollama import OllamaEmbeddings
+from langchain_huggingface.embeddings import HuggingFaceEmbeddings
 import tomllib
 
 def get_embedding_function(base_url="http://localhost:11434", model="nomic-embed-text"):
-    # embeddings = BedrockEmbeddings(
-    #     credentials_profile_name="default", region_name="us-east-1"
-    # )
-    embeddings = OllamaEmbeddings(base_url=base_url, model=model)
+    # embeddings = OllamaEmbeddings(base_ulsrl=base_url, model=model)
+    #model_name = "mixedbread-ai/mxbai-embed-large-v1"
+    model_name = "Alibaba-NLP/gte-large-en-v1.5" # better than above
+    # model_name = "allenai/scibert_scivocab_uncased" # note a sentence-transformer
+    model_kwargs = {'device': 'cuda',
+        "trust_remote_code":True,}
+    encode_kwargs = {'normalize_embeddings': True}
+    embeddings = HuggingFaceEmbeddings(
+        cache_folder="/vast/scratch/users/yang.e/langchain-cache",
+        model_name=model_name,
+        model_kwargs=model_kwargs,
+        encode_kwargs=encode_kwargs
+    )
     return embeddings
 
 
