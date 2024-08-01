880343de06e59cf0dfcdf5dce54a20e8fb42b3bf
Edward Yang <yang.e@wehi.edu.au>
2024-08-01 17:38:41 +1000

    add ood app files

--- chatbot.py
+++ chatbot.py
@@ -1,4 +1,5 @@
 import gradio
 import query_data
+import socket
 
-gradio.ChatInterface(query_data.query_rag).launch()
+gradio.ChatInterface(query_data.query_rag).launch(ssl_verify=False, server_name=socket.gethostname(), root_path=f"/node/{socket.gethostname()}/7860")
