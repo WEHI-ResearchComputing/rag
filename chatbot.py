103071f651f0e1ebbfb1c03ec80f72ffe88cd44e
Edward Yang <yang.e@wehi.edu.au>
2024-08-02 16:52:00 +1000

    add theme and default models

--- chatbot.py
+++ chatbot.py
@@ -9,12 +9,19 @@ parser.add_argument("--port", type=int, help="The port that the app will run on"
 parser.add_argument("--host", type=str, help="The host the app is running on", default=socket.gethostname())
 args = parser.parse_args()
 
-with gradio.Blocks() as demo:
+thee = gradio.themes.Default(
+    primary_hue="blue",
+    secondary_hue="green",
+    font=["Arial", "sans-serif"]
+)
+
+with gradio.Blocks(theme=thee) as demo:
     with gradio.Group():
         with gradio.Row():
             data_path = gradio.Textbox(label="Data Path")
             embedding_model = gradio.Dropdown(
                 ["mxbai-embed-large", "nomic-embed-text", "snowflake-arctic-embed:22m"], 
+                value="mxbai-embed-large",
                 label="Embedding Model"
             )
             add_data_btn = gradio.Button("Add Data to Database")
@@ -27,10 +34,19 @@ with gradio.Blocks() as demo:
     with gradio.Group():
         llm_model = gradio.Dropdown(
             ["mistral", "mistral-nemo", "phi3:mini", "phi3:medium"],
+            value="mistral",
             label="LLM"
         )
 
-        gradio.ChatInterface(query_data.query_rag, additional_inputs=[llm_model, embedding_model])
+        gradio.ChatInterface(
+            query_data.query_rag, 
+            title = "WEHI Local GPT",
+            description = "A Local LLM that can answer questions about your text-based data!",
+            undo_btn = None,
+            clear_btn = None,
+            fill_height = True,
+            additional_inputs=[llm_model, embedding_model]
+        )
 
 demo.launch(
     ssl_verify=False, 
