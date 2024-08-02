a72487ff3ce172b5226416d93842475c68beef70
Edward Yang <yang.e@wehi.edu.au>
2024-08-02 16:02:09 +1000

    update ood app to use available port

--- template/after.sh
+++ template/after.sh
@@ -1,11 +1,11 @@
 # Wait for the Jupyter Notebook server to start
-echo "Waiting for Jupyter Notebook server to open port 7860..."
+echo "Waiting for Jupyter Notebook server to open port $port..."
 echo "TIMING - Starting wait at: $(date)"
-if wait_until_port_used "${host}:7860" 60; then
-  echo "Discovered Jupyter Notebook server listening on port 7860"
+if wait_until_port_used "${host}:$port" 60; then
+  echo "Discovered Jupyter Notebook server listening on port $port"
   echo "TIMING - Wait ended at: $(date)"
 else
-  echo "Timed out waiting for Jupyter Notebook server to open port 7860!"
+  echo "Timed out waiting for Jupyter Notebook server to open port $port!"
   echo "TIMING - Wait ended at: $(date)"
   pkill -P ${SCRIPT_PID}
   clean_up 1
